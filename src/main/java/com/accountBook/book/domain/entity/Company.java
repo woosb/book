87a25b524a976352605d4f8ce8e32b8ef4551997
wsb <sbwoo@github.com>
2022-09-05 22:54:56 +0900

    2022.09.05 jpa 연관관계 시 순환참조 에러 해결 방식
    양방향 맵핑 시 entity를 바로 response로 반환하게 되면 순환참조가 일어나게 된다.(ex toString) 때문에
    @JsonManagedReference  - 일
    @JsonBackReference  - 다
    어노테이션을 이용하는 방법도 있지만
    dto객체를 만들어서 반환하는것이 더 좋다.
    entity는 변경될 여지가 있지만 api 명세상 반환하는 내용이 변하게 된다면 api를 신뢰할 수 없게 되고 나중에 되돌릴 수 없는 상황이 발생할 수 있기 때문이다.

--- src/main/java/com/accountBook/book/domain/entity/Company.java
+++ src/main/java/com/accountBook/book/domain/entity/Company.java
@@ -26,7 +26,14 @@ public class Company {
     private Long CompanyId;
 
     @OneToMany(mappedBy = "company", cascade= CascadeType.ALL)
-//    @JsonManagedReference
+    /*
+    * 양방향 맵핑 시 entity를 바로 response로 반환하게 되면 순환참조가 일어나게 된다.(ex toString) 때문에
+    * @JsonManagedReference
+    * 어노테이션을 이용하는 방법도 있지만
+    * dto객체를 만들어서 반환하는것이 더 좋다.
+    * entity는 변경될 여지가 있지만 api 명세상 반환하는 내용이 변하게 된다면 api를 신뢰할 수 없게 되고 나중에 되돌릴 수 없는 상황이 발생할 수 있기 때문이다.
+    *
+    * */
     public List<FinancialPosition> financialPositionList = new ArrayList<>();
 
     private String code;
