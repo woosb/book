a72e974c7f4f6a6e23d941ee2f3238db0fee3bd5
wsb <woosb0115@nate.com>
2022-09-24 23:35:26 +0900

    2022.09.24 financialPositionDtos는 한번에 조회하지 않도록 수정 변수명 오타 수정

--- src/main/java/com/accountBook/book/dto/CompanyDto.java
+++ src/main/java/com/accountBook/book/dto/CompanyDto.java
@@ -4,19 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.List;
-
 @Builder
 @Data
 @AllArgsConstructor
 public class CompanyDto {
 
-    private Long CompanyId;
+    private Long companyId;
     private String code;
     private String name;
     private String kindOfMarket;
     private String sectors_code;
     private String sectors_name;
-    private List<FinancialPositionDto> financialPositionDtos;
 
 }
