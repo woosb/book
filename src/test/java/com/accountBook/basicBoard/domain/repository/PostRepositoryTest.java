7b0b98890a302064e14b351ab508deb3b7ff597f
wsb <woosb0115@nate.com>
2022-11-07 21:58:18 +0900

    2022.11.07 Post CRUD 테스트

--- src/test/java/com/accountBook/basicBoard/domain/repository/PostRepositoryTest.java
+++ src/test/java/com/accountBook/basicBoard/domain/repository/PostRepositoryTest.java
@@ -1,49 +1,106 @@
 package com.accountBook.basicBoard.domain.repository;
 
-import com.accountBook.basicBoard.domain.entity.Comment;
 import com.accountBook.basicBoard.domain.entity.Post;
 import com.accountBook.basicBoard.domain.entity.TimeEntity;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Optional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PostRepositoryTest {
 
     @Autowired
     private PostRepository postRepository;
 
     @Test
-    void savePostAndComments() {
-        Post post;
-        TimeEntity timeEntity = new TimeEntity();
-        timeEntity.setInsertDate(LocalDateTime.now());
-        timeEntity.setUpdateDate(LocalDateTime.now());
-        post = Post.builder()
+    @Order(1)
+    @Rollback(value = false)
+    @DisplayName("save_post")
+    public void savePost() {
+        //given
+        TimeEntity timeEntity = new TimeEntity(LocalDateTime.now(), LocalDateTime.now());
+
+        Post post = Post.builder()
                 .title("testTitle")
                 .content("TestContents")
                 .timeEntity(timeEntity)
                 .comments(new ArrayList<>())
                 .viewCount(1).build();
 
-        post.getComments().add(Comment.builder().content("testContents1").timeEntity(timeEntity).post(post).build());
-        post.getComments().add(Comment.builder().content("testContents2").timeEntity(timeEntity).post(post).build());
-        post.getComments().add(Comment.builder().content("testContents3").timeEntity(timeEntity).post(post).build());
-        post.getComments().add(Comment.builder().content("testContents4").timeEntity(timeEntity).post(post).build());
+        //when
+        postRepository.save(post);
 
-        Post save = postRepository.save(post);
-        Assertions.assertEquals(post, save);
+        //then
+        Assertions.assertThat(post.getPostId()).isGreaterThan(0);
+    }
 
-        postRepository.delete(post);
-        long count = postRepository.count();
-        Assertions.assertEquals(count, 0);
+    @Test
+    @Order(2)
+    @Rollback(value = false)
+    @DisplayName("find_post")
+    public void findPost() {
+        //given
+        Optional<Post> postByPostId = postRepository.findPostByPostId(1L);
+
+        //when
+        Post post = postByPostId.orElseThrow();
+
+        //then
+        Assertions.assertThat(post.getPostId()).isEqualTo(1L);
+    }
+
+    @Test
+    @Order(3)
+    @Rollback(value = false)
+    @DisplayName("update_post")
+    public void updatePost(){
+        //given
+        Optional<Post> postByPostId = postRepository.findPostByPostId(1L);
+        Post post = postByPostId.orElseThrow();
+
+        //when
+        post.setTitle("updatedTitle");
+        post.setContent("updatedContent");
+        TimeEntity timeEntity = post.getTimeEntity();
+        LocalDateTime updatedTime = LocalDateTime.now();
+        timeEntity.setUpdateDate(updatedTime);
+        postRepository.save(post);
+
+        //then
+        Post updatedPost = postRepository.findPostByPostId(1L).orElseThrow();
+        Assertions.assertThat(updatedPost.getTitle()).isEqualTo("updatedTitle");
+        Assertions.assertThat(updatedPost.getContent()).isEqualTo("updatedContent");
+        Assertions.assertThat(updatedPost.getTimeEntity().getUpdateDate()).isEqualTo(updatedTime);
+    }
+
+
+    @Test
+    @Order(3)
+    @Rollback(value = false)
+    @DisplayName("delete_post")
+    public void deletePost(){
+        //given
+        Optional<Post> findPost = postRepository.findPostByPostId(1L);
+        if(findPost.isPresent()){
+            Post post = findPost.get();
+            postRepository.delete(post);
+        }
+
+        //when
+        Post tempPost = null;
+        Optional<Post> optionalPost = postRepository.findPostByPostId(1L);
+        if(optionalPost.isPresent()){
+            tempPost = optionalPost.get();
+        }
+
+        //then
+        Assertions.assertThat(tempPost).isNull();
     }
 }
